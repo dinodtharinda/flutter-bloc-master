f5721569e37e2580eec378c8ac5b574acc4adb18
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-10-03 00:17:25 +0530

    supabase initilized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_master/core/secrets/app_secrets.dart';
 import 'package:flutter_bloc_master/core/theme/theme.dart';
 import 'package:flutter_bloc_master/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnnonkey,
+  );
   runApp(const MyApp());
 }
 
